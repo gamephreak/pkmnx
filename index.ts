703b513d73e9df4eee2a5f107cac2cf6963d2e27
Kirk Scheibelhut <kjs@google.com>
2019-01-18 16:25:08 -0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,2 @@
+export {Abilities, Ability, Aliases, Boost, BoostsTable, Gender, Generation, Generations, ID, Move, Moves, Nature, Natures, Pokedex, PokemonSet, Sets, Stat, Stats, StatsTable, Team, Teams, Tier, Tiers, toID, Type, TypeChart, Types} from 'pkmn';
+export {EventInfo, Species} from './species';  // TODO Pokedex, Pokemon?
---
+++ species.ts
@@ -0,0 +1,49 @@
+import * as pkmn from 'pkmn';
+
+/**
+ * Describes a possible way to get a move onto a pokemon.
+ *
+ * First character is a generation number, 1-7.
+ * Second character is a source ID, one of:
+ *
+ * - L = start or level-up, 3rd char+ is the level
+ * - M = TM/HM
+ * - T = tutor
+ * - E = egg
+ * - S = event, 3rd char+ is the index in .eventPokemon
+ * - D = Dream World, only 5D is valid
+ * - V = Virtual Console transfer, only 7V is valid
+ * - C = NOT A REAL SOURCE, see note, only 3C/4C is valid
+ *
+ * C marks certain moves learned by a pokemon's prevo. It's used to
+ * work around the chainbreeding checker's shortcuts for performance;
+ * it lets the pokemon be a valid father for teaching the move, but
+ * is otherwise ignored by the learnset checker (which will actually
+ * check prevos for compatibility).
+ */
+type MoveSource = string;
+
+export type EventInfo = {
+  readonly generation: number;
+  readonly level?: number;
+  readonly shiny?: true | 1;
+  readonly gender?: pkmn.Gender;
+  readonly nature?: string;
+  readonly ivs?: Readonly<Partial<pkmn.StatsTable>>;
+  readonly perfectIVs?: number;
+  readonly isHidden?: boolean;
+  readonly abilities?: Readonly<string[]>;
+  readonly moves?: Readonly<string[]>;
+  readonly pokeball?: string;
+  readonly from?: string;
+};
+
+export interface Species extends pkmn.Species {
+  readonly eggGroups: Readonly<string[]>;
+  readonly evoLevel?: number;
+  readonly maleOnlyHidden?: boolean;
+  readonly unreleasedHidden?: boolean;
+  readonly eventOnly: boolean;
+  readonly eventPokemon?: Readonly<EventInfo[]>;
+  readonly learnset?: Readonly<{[k: string]: MoveSource[]}>;
+}
