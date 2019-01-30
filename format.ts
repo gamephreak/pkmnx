da456f1daac0252c7a2e2b5c3888a47f76c0f866
Kirk Scheibelhut <kjs@scheibo.com>
2019-01-29 20:02:22 -0800

    Add format, think about sets

---
+++ format.ts
@@ -0,0 +1,23 @@
+import * as pkmn from 'pkmn';
+
+export class Format {
+  constructor(readonly gen: pkmn.Generation = 7, readonly tier: pkmn.Tier = 'OU') {
+    this.gen = gen;
+    this.tier = tier;
+  }
+
+  toString() {
+    return `${gen}${tier}`
+  }
+
+  static fromString(s: string): Format|undefined {
+    // BUG: this will break in ~10 years when gen10 gets released...
+    if (s.slice(0, 3) === 'gen') {
+      const tier: pkmn.Tier|undefined = Tiers.fromString(s.slice(4));
+      return tier ? new Format(Number(s[3])) as pkmn.Generation, tier) : undefined;
+    } else {
+      const tier: pkmn.Tier|undefined = Tiers.fromString(s);
+      return tier ? new Format(6, tier) : undefined;
+    }
+  }
+};
